21342b058f9c9be3a15e0aa70fc9715efd5385db
Vinicius P Stormoski <vinicius.stormoski@conectanuvem.com.br>
2024-03-26 08:52:02 -0300

    FEAT: adicionado map dos valores e exibição no componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Estudar react",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ])
 
-  return (
-    <>
-      <div>
-        <p>
-          Teste
-        </p>
-      </div>
-    </>
-  )
+  return <div className='app'>
+    <h1>Lista de Tarefas</h1>
+    <div className='todo-list'>
+      {todos.map((todo) => (
+        <div className='todo'>
+          <div className='content'>
+            <p>{todo.text}</p>
+            <p className='category'>{todo.category}</p>
+          </div>
+          <div>
+            <button>Completar</button>
+            <button>X</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
 }
 
 export default App
